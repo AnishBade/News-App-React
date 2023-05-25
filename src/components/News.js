bbf594abd60206c898db7965d4575c60fda02ad5
AnishBade <badeanish6045@gmail.com>
2023-05-25 15:33:49 +0545

    state in class based components

--- src/components/News.js
+++ src/components/News.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
+  constructor(){
+    super();
+    console.log("Hello I am a constructor from News Component")
+ }
   render() {
     return (
       <div className='container my-3'>
         <h2>NewsMonkey - Top Headlines</h2>
         <div className="row">
             <div className="col-md-4">
-              <NewsItem title="myTitle" description="mydesc"/>
+              <NewsItem title="myTitle" description="mydesc" imageUrl="" />
             </div>
             <div className="col-md-4">
               <NewsItem title="myTitle" description="mydesc"/>
